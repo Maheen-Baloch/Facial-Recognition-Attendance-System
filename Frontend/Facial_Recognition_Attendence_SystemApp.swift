42335da190bbac697576cd439325d8b242c86a47
Maheen Baloch <maheenbaloch889@gmail.com>
2026-02-23 22:31:29 +0500

    Add files via upload

---
+++ Frontend/Facial_Recognition_Attendence_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Facial_Recognition_Attendence_SystemApp.swift
+//  Facial Recognition Attendence System
+//
+//  Created by Maheen Baloch on 13/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Facial_Recognition_Attendence_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
